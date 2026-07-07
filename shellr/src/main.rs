3e04659add9eeba7cfc6bff2036228747da8edec
ctrlaltsam <spencersama0@gmail.com>
2026-07-07 01:16:56 -0400

    Base + ls

---
+++ shellr/src/main.rs
@@ -0,0 +1,82 @@
+use std::env;
+use std::io::{stdin, stdout, Write};
+use std::path::Path;
+use std::process::{Child, Command, Stdio};
+use std::fs;
+
+fn main() {
+    loop {
+        print!("{}> ", env::current_dir().unwrap().display());
+        stdout().flush();
+        
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut previous_command = None;
+
+        while let Some(command) = commands.next() {
+            let mut parts = input.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                    let root = Path::new(new_dir);
+                    if let Err(e) = env::set_current_dir(&root){
+                        eprintln!("{}", e);
+                    }
+                },
+
+                "ls" => {
+                    let paths = fs::read_dir(env::current_dir().unwrap()).unwrap();
+
+                    for path in paths {
+                        println!("{}", path.unwrap().file_name().to_string_lossy())
+                    }
+                },
+
+                "exit" => return,
+
+                command => {
+                    let stdin = previous_command
+                        .map_or(
+                            Stdio::inherit(),
+                             |output: Child| Stdio::from(output.stdout.unwrap())
+                            );
+
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    }
+                    else{
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => {
+                            previous_command = Some(output);
+                        },
+                        Err(e) => {
+                            previous_command = None;
+                            eprintln!("{}", e);
+                        },
+                    };
+                }
+            }
+        }
+
+        if let Some(mut final_command) = previous_command {
+            final_command.wait();
+        }
+
+
+    }
+
+}
